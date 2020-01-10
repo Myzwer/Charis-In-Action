ac72919162257cab84dd501ee71368dfa9bafb85
Josh Forrester <twicearoundmars@yahoo.com>
2020-01-10 09:25:30 -0500

    Created the content for the Donate page

--- app/page-donate.php
+++ app/page-donate.php
@@ -16,14 +16,13 @@
 
 get_header(); ?>
 
-<div class = "donate-header">
+<div class = "">
     <div class="grid-container fullWidth">
         <div class="grid-x grid-padding-x">
-            <div class="large-12 cell billboard-main">
+            <div class="small-12 cell billboard-main donate-header">
                 <div class = "center">
                     <h2>Give to the Mission</h2>
-                    <hr>
-                    <h3>Maecenas vel purus sit amet nibh dictum suscipit sit amet eu metus, nunc justo purus.</h3>
+                    <a class = "ff dark1" href = "#">Donate Now <i class="fas fa-arrow-circle-right push"></i></a>
                 </div>
             </div>
         </div>
@@ -31,9 +30,69 @@ get_header(); ?>
 </div>
 
 
-<div class="grid-container fullWidth">
-    <div class="grid-x grid-padding-x">
-        <div class="large-12 cell">
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x grid-margin-x">
+            <div class="small-12 cell donate-card">
+                <h2>Who am I helping?</h2>
+                <p>People want to know where their money is going. Tell them.  Donec suscipit tincidunt neque eu euismod. Suspendisse molestie nunc id fermentum consequat. Nunc convallis massa sit amet augue convallis eleifend. Proin sodales malesuada dolor, ac hendrerit ipsum interdum feugiat. Phasellus gravida urna ligula, at aliquam est sodales vitae. Donec suscipit arcu lacus, nec suscipit tellus pellentesque vitae. Pellentesque vitae ligula gravida, maximus ligula et, malesuada erat.</p>
+            </div>
         </div>
     </div>
-</div>
+
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="small-12 cell ">
+                <img class = "partner-4" src="http://charis-in-action.local/wp-content/uploads/2020/01/photo-1518709766631-a6a7f45921c3.jpeg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-container fullWidth">
+        <div class="grid-x">
+            <div class="small-12 cell ">
+                <div class="small-12 cell donate-banner">
+                    <h2>Our Partners for Donations</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--<div class="grid-container fullWidth">
+        <div class="grid-x grid-margin-x">
+            <div class="small-12 medium-6 medium-offset-3 large-4 large-offset-0 image-container cell">
+                <img src="http://charis-in-action.local/wp-content/uploads/2020/01/FC-Headshots-8118.jpg" alt="">
+            </div>
+                <div class="small-12 medium-6 medium-offset-3 large-8 large-offset-0 cell donate-partner-card">
+                    <h2>Todd and Kristy</h2>
+                    <p>Donec suscipit tincidunt neque eu euismod. Suspendisse molestie nunc id fermentum consequat. Nunc convallis massa sit amet augue convallis eleifend. Proin sodales malesuada dolor, ac hendrerit ipsum interdum feugiat. Phasellus gravida urna ligula, at aliquam est sodales vitae. Donec suscipit arcu lacus, nec suscipit tellus pellentesque vitae. Pellentesque vitae ligula gravida, maximus ligula et, malesuada erat.</p>
+                    <div class="button-center">
+                        <a class = "ff dark1" href = "#">Donate Now <i class="fas fa-arrow-circle-right push"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>-->
+
+    <div class="grid-container fullWidth">
+        <div class="grid-x grid-margin-x">
+            <div class="small-12 medium-6 large-4 cell">
+
+                <div class="grid-container fullWidth">
+                    <div class="grid-x grid-margin-x">
+                        <div class="small-12 image-container cell">
+                            <img src="http://charis-in-action.local/wp-content/uploads/2020/01/FC-Headshots-8118.jpg" alt="">
+                        </div>
+                        <div class="small-12 cell donate-partner-card">
+                            <h2>Todd and Kristy</h2>
+                            <p>Donec suscipit tincidunt neque eu euismod. Suspendisse molestie nunc id fermentum consequat. Nunc convallis massa sit amet augue convallis eleifend. Proin sodales malesuada dolor, ac hendrerit ipsum interdum feugiat. Phasellus gravida urna ligula, at aliquam est sodales vitae. Donec suscipit arcu lacus, nec suscipit tellus pellentesque vitae. Pellentesque vitae ligula gravida, maximus ligula et, malesuada erat.</p>
+                            <div class="button-center">
+                                <a class = "ff dark1" href = "#">Donate Now <i class="fas fa-arrow-circle-right push"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer();
