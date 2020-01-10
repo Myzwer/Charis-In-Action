6da22ee2ddb3a95e118deefa4c40fbbd68bf8dc8
Josh Forrester <twicearoundmars@yahoo.com>
2020-01-10 11:18:31 -0500

    Contact page has been fully built out.

--- app/page-contact.php
+++ app/page-contact.php
@@ -26,7 +26,32 @@ get_header(); ?>
     </div>
 </div>
 
+    <div class="grid-container contact-body">
+        <div class="grid-x grid-padding-x grid-margin-x">
+            <div class="small-12 medium-6 large-6 cell contact-form">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                    the_content();
+                endwhile; else: ?>
+                    <p>Sorry, no posts matched your criteria.</p>
+                <?php endif; ?>
+            </div>
+            <div class="small-12 medium-6 large-6 cell  contact-info">
+                <h3>Reach out and stuff.</h3>
+                <p>Donec arcu nunc, feugiat et venenatis ornare, laoreet sit amet erat. Nunc sed ipsum ultricies, cursus mi a, mattis ante. Integer id mollis ligula. Nunc velit velit, tempus vel accumsan at, dictum eu nisi. Integer accumsan sapien sed diam tempus, molestie vestibulum ligula ultrices. Nullam ac viverra quam. Aliquam sagittis enim vitae eleifend placerat. Morbi et elit sed libero venenatis iaculis.</p>
+            </div>
+        </div>
+    </div>
 
-
+    <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+            <div class="small-12 cell contact-image-banner ">
+                <img class = "partner-4" src="http://charis-in-action.local/wp-content/uploads/2020/01/frontpage-contact.jpeg" alt="">
+            </div>
+            <div class="small-12 cell email-banner">
+                <h3>Email work better?</h3>
+                <p>Hey, that's fine probably. Curabitur dignissim metus sed elit accumsan, sed ornare velit sodales. Integer nec dapibus ligula. Sed et neque vel purus commodo <a href="mailto:info@charisinaction.org">info@charisinaction.org</a>. </p>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer();
